f9b0e2c0e0c01fe499c73e3f282de826fb76daad
Elizabeth <elizabeth.a.dinella@gmail.com>
2021-12-10 18:31:16 -0500

    added train and command line arg for data directory

--- mask-rcnn/train.py
+++ mask-rcnn/train.py
@@ -1,20 +1,18 @@
 import torch
-import os, json, cv2, random, argparse, sys
-from glob import glob
-from tqdm import tqdm
-
 import detectron2
 from detectron2.utils.logger import setup_logger
 from detectron2.structures import BoxMode
-from detectron2.checkpoint import DetectionCheckpointer
+import numpy as np
+import os, json, cv2, random, sys
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.data import build_detection_test_loader
-from detectron2.utils.visualizer import ColorMode
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.engine import DefaultTrainer
+from glob import glob
+from tqdm import tqdm
 
 if(len(sys.argv) < 2): 
     print("USAGE: python train.py DATA_DIR")
@@ -34,19 +32,18 @@ def setup_cfg():
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
     cfg.DATASETS.TRAIN = ("washertrain",)
-    cfg.DATASETS.TEST = ("washertest",)
+    cfg.DATASETS.TEST = ()
     cfg.DATALOADER.NUM_WORKERS = 2
-    cfg.TEST.DETECTIONS_PER_IMAGE = 1000
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
     cfg.SOLVER.IMS_PER_BATCH = 2
-    cfg.SOLVER.STEPS = []        
+    cfg.SOLVER.BASE_LR = 0.00025  
+    cfg.SOLVER.MAX_ITER = 9000    
+    cfg.SOLVER.STEPS = []        # do not decay learning rate
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (washer)
-
-    cfg.MODEL.WEIGHTS = os.path.join("output", "model_final.pth")  # path to the model we just trained
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class 
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
 
     return cfg
-
 
 def get_washer_dicts(img_dir):
     json_file = os.path.join(img_dir, "annotations.json")
@@ -89,63 +86,23 @@ def get_washer_dicts(img_dir):
         dataset_dicts.append(record)
     return dataset_dicts
 
-def get_label(_id, imgs_anns):
-    annos = [s for s in imgs_anns["annotations"] if s["image_id"] == _id]
-    return len(annos)
-
-
-def visualize_predictions(predictor, metadata):
-    #randomly select test samples to visulaize
-    dataset_dicts = get_washer_dicts(os.path.join(DATA_DIR, "test"))
-    for d in random.sample(dataset_dicts, 3):    
-        im = cv2.imread(d["file_name"])
-        outputs = predictor(im)  
-        v = Visualizer(im[:, :, ::-1],
-                       metadata=metadata, 
-                       scale=0.5
-        )
-        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        outname = os.path.join("preds", d["file_name"][d["file_name"].rfind("/")+1:])
-        print("Output for prediction", outname, ": ", len(outputs["instances"]))
-        cv2.imwrite(outname, out.get_image())
-
-
-def calculate_metrics(predictor, test_loader):
-    json_file = os.path.join(DATA_DIR, "annotations.json")
-    with open(json_file) as f:
-        imgs_anns = json.load(f)
-
-
-    MAE, MSE, total = 0, 0, 0
-    
-    for sample in tqdm(test_loader):
-        output = predictor(cv2.imread(sample[0]["file_name"]))
-        count = len(output["instances"])
-        label = get_label(sample[0]["id"], imgs_anns)
-
-        MAE += abs(count - label)
-        MSE += (label - count)**2
-        total += 1
-
-    return MAE / total, MSE / total
-
-
-def main():
+def main(): 
     for d in ["train", "val", "test"]:
         DatasetCatalog.register("washer" + d, lambda d=d: get_washer_dicts(os.path.join(DATA_DIR, d)))
         MetadataCatalog.get("washer" + d).thing_classes = ["washer"]
 
     washer_metadata = MetadataCatalog.get("washer_train")
+
     cfg = setup_cfg()
     predictor = DefaultPredictor(cfg)
-    test_loader = build_detection_test_loader(cfg, "washertest")
 
-    visualize_predictions(predictor, washer_metadata)
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    trainer = DefaultTrainer(cfg) 
+    trainer.resume_or_load(resume=False)
+    trainer.train()
 
-    MAE, MSE = calculate_metrics(predictor, test_loader)
-    print("Mean average error: {:2.4f}".format(MAE))
-    print("Mean squared error: {:2.4f})".format(MSE))
-
+    checkpointer = DetectionCheckpointer(predictor.model, save_dir="output")
+    checkpointer.save("model")  # save to output/model_999.pth
 
 if __name__ == '__main__':
     main()
